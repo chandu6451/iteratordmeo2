f5303bba9ae76ddf1375152f344b57a659ef60db
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 14:54:40 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorDemo2
+{
+    public class DayofTheWeek
+    {
+        string[] m_day = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+        public IEnumerable<string> GetDays()
+        {
+            for (int i = 0; i < m_day.Length; i++)
+            {
+                yield return m_day[i];
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DayofTheWeek week = new DayofTheWeek();
+
+            foreach (var item in week.GetDays())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
